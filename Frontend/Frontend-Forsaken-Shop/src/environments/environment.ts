b3d2b89a85284d9bae7954a52c5cc331208f89a6
fr.garaym@duocuc.cl <fr.garaym@duocuc.cl>
2026-05-18 05:21:56 -0400

    frontend: configurar proyecto angular y proxy

---
+++ Frontend/Frontend-Forsaken-Shop/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  api: {
+    categorias: '/api/categorias',
+    roles: '/api/roles',
+    usuarios: '/api/usuarios',
+    prendas: '/api/prendas',
+    ventas: '/api/ventas',
+    detallesVentas: '/api/detalles-ventas',
+    mensajes: '/api/mensajes',
+  },
+};
